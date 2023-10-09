b1d8486714a4dfd9be4fdfcda5a61dc40ca0860d
Vincent Guittot <vincent.guittot@linaro.org>
2023-10-09 12:36:16 +0200

    sched/topology: Consolidate and clean up access to a CPU's max compute capacity
    [ Upstream commit 7bc263840bc3377186cb06b003ac287bb2f18ce2 ]
    
    Remove the rq::cpu_capacity_orig field and use arch_scale_cpu_capacity()
    instead.
    
    The scheduler uses 3 methods to get access to a CPU's max compute capacity:
    
     - arch_scale_cpu_capacity(cpu) which is the default way to get a CPU's capacity.
    
     - cpu_capacity_orig field which is periodically updated with
       arch_scale_cpu_capacity().
    
     - capacity_orig_of(cpu) which encapsulates rq->cpu_capacity_orig.
    
    There is no real need to save the value returned by arch_scale_cpu_capacity()
    in struct rq. arch_scale_cpu_capacity() returns:
    
     - either a per_cpu variable.
    
     - or a const value for systems which have only one capacity.
    
    Remove rq::cpu_capacity_orig and use arch_scale_cpu_capacity() everywhere.
    
    No functional changes.
    
    Some performance tests on Arm64:
    
      - small SMP device (hikey): no noticeable changes
      - HMP device (RB5):         hackbench shows minor improvement (1-2%)
      - large smp (thx2):         hackbench and tbench shows minor improvement (1%)
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Link: https://lore.kernel.org/r/20231009103621.374412-2-vincent.guittot@linaro.org
    Stable-dep-of: 79443a7e9da3 ("cpufreq/sched: Explicitly synchronize limits_changed flag handling")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1133,7 +1133,6 @@ struct rq {
 	struct sched_domain __rcu	*sd;
 
 	unsigned long		cpu_capacity;
-	unsigned long		cpu_capacity_orig;
 
 	struct balance_callback *balance_callback;
 
@@ -3144,11 +3143,6 @@ static inline void cpufreq_update_util(struct rq *rq, unsigned int flags) {}
 #endif
 
 #ifdef CONFIG_SMP
-static inline unsigned long capacity_orig_of(int cpu)
-{
-	return cpu_rq(cpu)->cpu_capacity_orig;
-}
-
 /**
  * enum cpu_util_type - CPU utilization type
  * @FREQUENCY_UTIL:	Utilization used to select frequency
