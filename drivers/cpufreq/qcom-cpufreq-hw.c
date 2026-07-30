d6a9c127968a715e50d8b18135cc7b3e2774cb90
xkse4 <xkse4@outlook.com>
2026-07-30 16:01:10 +0800

    cpufreq: qcom-cpufreq-hw: fix devm data double free

--- drivers/cpufreq/qcom-cpufreq-hw.c
+++ drivers/cpufreq/qcom-cpufreq-hw.c
@@ -583,7 +583,6 @@ static int qcom_cpufreq_hw_cpu_exit(struct cpufreq_policy *policy)
 	dev_pm_opp_of_cpumask_remove_table(policy->related_cpus);
 	qcom_cpufreq_hw_lmh_exit(data);
 	kfree(policy->freq_table);
-	kfree(data);
 
 	return 0;
 }
